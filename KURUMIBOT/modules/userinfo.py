f499f46f7f33be0f61a85918490277e6a469e3e2
natsu-dragneel13 <82117289+natsu-dragneel13@users.noreply.github.com>
2021-05-14 21:01:16 +0300

    Update userinfo.py

--- KURUMIBOT/modules/userinfo.py
+++ KURUMIBOT/modules/userinfo.py
@@ -516,7 +516,7 @@ Examples:
  • `/info`*:* get information about a user. 
  
 *What is that health thingy?*
- Come and see [HP System explained](https://t.me/OnePunchUpdates/192)
+ Come and see [HP System explained](https://t.me/KURUMIBOT_SUPPORT/16)
 """
 
 SET_BIO_HANDLER = DisableAbleCommandHandler("setbio", set_about_bio)
