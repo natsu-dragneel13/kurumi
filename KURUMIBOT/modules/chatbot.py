d4408efc636cac12780bf78e81fff0c57abfad51
natsu-dragneel13 <82117289+natsu-dragneel13@users.noreply.github.com>
2021-05-15 12:28:14 +0300

    Update chatbot.py

--- KURUMIBOT/modules/chatbot.py
+++ KURUMIBOT/modules/chatbot.py
@@ -150,8 +150,8 @@ Chatbot utilizes the CoffeeHouse API and allows Saitama to talk and provides a m
  • `/addchat`*:* Enables Chatbot mode in the chat.
  • `/rmchat`*:* Disables Chatbot mode in the chat.
 
-Reports bugs at @{SUPPORT_CHAT}
-*Powered by CoffeeHouse* (https://coffeehouse.intellivoid.net/) from @Intellivoid
+Reports bugs at @kurumi_BOT_SUPPORT
+*Powered by [CoffeeHouse](https://coffeehouse.intellivoid.net/) from @Intellivoid
 """
 
 ADD_CHAT_HANDLER = CommandHandler("addchat", add_chat)
